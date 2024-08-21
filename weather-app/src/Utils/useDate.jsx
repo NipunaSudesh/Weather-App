7313e2359927628112253a3169a68e54b703911e
Nipuna Sudesh <142614223+NipunaSudesh@users.noreply.github.com>
2024-08-21 11:33:16 +0530

    created use date components

---
+++ weather-app/src/Utils/useDate.jsx
@@ -0,0 +1,33 @@
+import { useEffect, useState } from 'react';
+
+export const useDate = () => {
+  const locale = 'en';
+  const [today, setToday] = useState(new Date());
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setToday(new Date());
+    }, 60 * 1000); 
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
+
+  const day = today.toLocaleDateString(locale, { weekday: 'long' });
+
+  const date = `${day}, ${today.getDate()} ${today.toLocaleDateString(locale, {
+    month: 'long',
+  })}`;
+
+  const time = today.toLocaleTimeString(locale, {
+    hour: 'numeric',
+    minute: 'numeric',
+    hour12: true,
+  });
+
+  return {
+    date,
+    time,
+  };
+};
