79ca24f123d1a5c1ca7f6e74f71c264f726504a6
Nipuna Sudesh <142614223+NipunaSudesh@users.noreply.github.com>
2024-08-21 15:47:38 +0530

    created nav bar

--- weather-app/src/App.jsx
+++ weather-app/src/App.jsx
@@ -1,11 +1,14 @@
 import './App.css'
+import { NavBar } from './Components/NavBar'
 
 function App() {
  
 
   return (
     <>
-       
+    <div className='flex flex-col gap-4 p-2'>
+      <NavBar />
+    </div>
     </>
   )
 }
---
+++ weather-app/src/Components/NavBar.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react'
+import { CiSearch } from "react-icons/ci";
+
+export const NavBar = () => {
+    const[input,setInput] =useState('');
+
+  return (
+    <div className='w-full h-screen text-white px-8'>
+    <nav className='w-full p-3 flex justify-between items-center'>
+      <h1 className='font-bold tracking-wide text-3xl text-black'>Weather App</h1>
+      <div className='bg-white w-[15rem] overflow-hidden shadow-2xl rounded flex item-center p-2 gap-2'>
+          <CiSearch className='text-black w-8 h-8'/>
+          <input type='text' 
+          placeholder='Search'
+            onKeyUp={(e)=>{
+                if(e.key=='Enter'){
+                    //Submit the form
+                }
+            }}
+
+          className='focus:outline-none w-full text-[#212121] text-lg' value={input} 
+          onChange={e=>setInput(e.target.value)}
+          />
+      </div>
+    </nav>
+
+
+ </div>
+  )
+}
