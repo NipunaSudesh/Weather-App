83332b31f1882a391bcb021ce1c2ea312791dced
Nipuna Sudesh <142614223+NipunaSudesh@users.noreply.github.com>
2024-08-22 11:12:45 +0530

    added weather png image

--- weather-app/src/assets/index.jsx
+++ weather-app/src/assets/index.jsx
@@ -6,6 +6,12 @@ import snow from './images/snow.jpg';
 import stormy2 from './images/stormy2.jpg';
 import sunny from './images/sunny.jpg';
 import wind from './images/wind.jpg';
+import fogPng from './images/fog.png';
+import rainPng from './images/rainpng.png';
+import sunnPng from './images/sunnyPng.png';
+import sunRainPng from './images/sunRain.png';
+import thunderPng from './images/thunder.png';
+
 
 export{
     clear,
@@ -15,5 +21,11 @@ export{
     snow,
     stormy2,
     sunny,
-    wind
+    wind,
+    fogPng,
+    rainPng,
+    sunnPng,
+    sunRainPng,
+    thunderPng
+
 }
