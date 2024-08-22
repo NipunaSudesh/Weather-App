874c0c4e185ecd169f8a90bdd9fddcb452215f9b
Nipuna Sudesh <142614223+NipunaSudesh@users.noreply.github.com>
2024-08-22 08:57:53 +0530

    added image

---
+++ weather-app/src/assets/index.jsx
@@ -0,0 +1,19 @@
+import clear from './images/clear.jpg';
+import cloudy from './images/cloudy.jpg';
+import fog from './images/fog.jpg';
+import rainy from './images/rainy.jpg';
+import snow from './images/snow.jpg';
+import stormy2 from './images/stormy2.jpg';
+import sunny from './images/sunny.jpg';
+import wind from './images/wind.jpg';
+
+export{
+    clear,
+    cloudy,
+    fog,
+    rainy,
+    snow,
+    stormy2,
+    sunny,
+    wind
+}
